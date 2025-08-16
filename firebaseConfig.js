2adafe985252ab0200bd49c90cfa5007eab4c731
Thabang Agondo <seventhlegendptyltd@gmail.com>
2025-08-17 01:06:00 +0200

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,12 @@
+
 // 🔥 Fixed Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCi1FrxDcZ_Yryiy0EDku8FIf_XlgJA6s0",
-  authDomain: "kasi-vibe-socials-0372.firebaseapp.com",
-  databaseURL: "https://kasi-vibe-socials-0372-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "kasi-vibe-socials-0372",
-  storageBucket: "kasi-vibe-socials-0372.appspot.com",
-  messagingSenderId: "719373156492",
-  appId: "1:719373156492:web:0e7b6c94363484960e7ba9"
+apiKey: "AIzaSyCi1FrxDcZ_Yryiy0EDku8FIf_XlgJA6s0",
+authDomain: "kasi-vibe-socials-0372.firebaseapp.com",
+databaseURL: "https://kasi-vibe-socials-0372-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "kasi-vibe-socials-0372",
+storageBucket: "kasi-vibe-socials-0372.appspot.com",
+messagingSenderId: "719373156492",
+appId: "1:719373156492:web:0e7b6c94363484960e7ba9"
 };
+firebase.initializeApp(firebaseConfig);
